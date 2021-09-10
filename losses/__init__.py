90600a909daf2fb5d43355262930b8694bd16808
Rodrigo Vidal <arodrigovidal@gmail.com>
2021-09-10 10:38:26 -0300

    __init__.py files are created

---
+++ losses/__init__.py
@@ -0,0 +1,2 @@
+from .losses import P_stamps_loss
+from .losses import NT_Xent
---
+++ models/__init__.py
@@ -0,0 +1,3 @@
+from .models import P_stamps_net
+from .models import SimCLR_net
+from .models import Linear_classifier
