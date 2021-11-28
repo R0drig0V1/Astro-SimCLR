8df4c5bc10797ac61e891d87ab47fc53743111a3
Rodrigo Vidal <arodrigovidal@gmail.com>
2021-11-28 16:51:07 -0300

    Multiple augmentation options are implemented

--- utils/dataset.py
+++ utils/dataset.py
@@ -9,6 +9,8 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from torch.utils.data.sampler import BatchSampler
 
+from utils.transformations import Resize_img
+
 # -----------------------------------------------------------------------------
 
 label_names = ['AGN', 'SN', 'VS', 'Asteroid', 'Bogus']
@@ -19,16 +21,32 @@ feature_names = ['sgscore1', 'distpsnr1', 'sgscore2', 'distpsnr2', 'sgscore3',
                  'ndethist', 'ncovhist', 'ecl_lat', 'ecl_long', 'gal_lat',
                  'gal_long', 'non_detections', 'chinr', 'sharpnr']
 
-
 # -----------------------------------------------------------------------------
 
 # Dataset is loaded
-with open('dataset/td_ztf_stamp_17_06_20.pkl', 'rb') as f:
-    data = pickle.load(f)
+#with open('dataset/td_ztf_stamp_17_06_20.pkl', 'rb') as f:
+#    data = pickle.load(f)
 
 # Mean and std to normalize
-mean_features = np.mean(np.array(features, dtype=np.float32), axis=0)
-std_features = np.std(np.array(features, dtype=np.float32), axis=0)
+#mean_features = np.mean(np.array(features, dtype=np.float32), axis=0)
+#std_features = np.std(np.array(features, dtype=np.float32), axis=0)
+
+
+mean_features = np.array([-0.2873714, 0.11175089, -0.03527644, -0.00055827,
+                          -0.00971938, 0.00157936, 0.5222817, 0.0454788,
+                          0.02194207, -0.04955371, -0.02466081, 0.02335262,
+                          -0.0385435, 0.01949287, 0.09752761, 0.14096506,
+                          0.09265201, -0.10308222, 0.02861715, 0.00802673,
+                          -0.02782944, -0.01558139, 0.00917627, 0.03677692,
+                          0.33868203, 0.2191507], dtype=np.float32)
+
+std_features = np.array([1.0836236, 1.0093966, 0.99515635, 0.9982329,
+                         0.996767, 0.994433, 0.91449946, 0.9764465,
+                         0.90470445, 0.964727,1.0097935, 0.9968355,
+                         0.9727092, 1.0060825, 1.0007532,0.9736594,
+                         0.95906055, 0.9262455, 1.0111927, 1.0210558,
+                         1.0055873, 1.0252767, 0.97880936, 1.0133779,
+                         1.332348, 1.254867], dtype=np.float32)
 
 # -----------------------------------------------------------------------------
 
@@ -48,33 +66,45 @@ def one_hot_trans(x):
 
 # -----------------------------------------------------------------------------
 
-# Class to load alerts
+# Class to load alerts of ZTF
 class Dataset_stamps(torch.utils.data.Dataset):
 
     def __init__(
             self,
             pickle,
             dataset,
-            transform=None,
+            image_size,
+            image_transformation=None,
+            image_original_and_augmentated=True,
             one_hot_encoding=True,
             discarted_features=[13,14,15]
             ):
 
+
         # Load parameters
         self.pickle = pickle
         self.dataset = dataset
+        self.additional_transformation = image_transformation is not None
+        self.image_original_and_augmentated = image_original_and_augmentated
         self.discarted_features = discarted_features
 
 
-        # Apply transformation for images
-        if (transform is None):
-            self.transform = transforms.Compose([transforms.Lambda(img_float2int),
-                                                transforms.ToPILImage()
-                                                ])
-        else:
-            self.transform = transforms.Compose([transforms.Lambda(img_float2int),
-                                                transforms.ToPILImage(),
-                                                transform])
+        # Base transformation for images
+        self.base_transformation = transforms.Compose([
+                transforms.Lambda(img_float2int),
+                transforms.ToPILImage(),
+                Resize_img(size=image_size)
+                ])
+
+
+        # Additional transformation
+        if (self.additional_transformation):
+
+            self.transformation = transforms.Compose([
+                transforms.Lambda(img_float2int),
+                transforms.ToPILImage(),
+                image_transformation
+                ])
 
 
         # Apply transformation for labels
@@ -107,11 +137,26 @@ class Dataset_stamps(torch.utils.data.Dataset):
         # Get labels
         label = self.pickle[self.dataset]['labels'][idx]
 
-        # Transformations are applied
-        image = self.transform(image)
+        # Base transformation for images is applied
+        image_b = self.base_transformation(image)
+
+        # Transformation for labels
         label = self.target_transform(label)
 
-        return image, feature, label
+
+        # Batch for simultaneous training of encoder and classifier
+        if (self.additional_transformation and self.image_original_and_augmentated):
+            image_t = self.transformation(image)
+            return image_b, image_t, feature, label
+
+        # Batch for stamps classifier with augmentations
+        elif (self.additional_transformation and self.image_original_and_augmentated==False):
+            image_t = self.transformation(image)[0]
+            return image_t, feature, label
+
+        # Batch for encoder training
+        else:
+            return image_b, feature, label
 
 # -----------------------------------------------------------------------------
 
