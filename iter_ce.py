01b82d5c611842692519bb74beac8163b29d129b
Rodrigo Vidal <arodrigovidal@gmail.com>
2022-03-16 15:05:31 -0300

    augmentations are applied to the best STAMPS model.

--- iter_ce.py
+++ iter_ce.py
@@ -1,13 +1,17 @@
 import os
+import yaml
+import shutil
 
 import numpy as np
+import pandas as pd
 import pytorch_lightning as pl
 
 from utils.config import config
 from utils.plots import plot_confusion_matrix_mean_std
 from utils.training import Supervised_Cross_Entropy
 
-from box import Box
+from utils.repeater_simclr_lib import hyperparameter_columns
+
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from pytorch_lightning.loggers import TensorBoardLogger
@@ -15,6 +19,36 @@ from pytorch_lightning.loggers import TensorBoardLogger
 # -----------------------------------------------------------------------------
 
 gpus = [3]
+
+# -----------------------------------------------------------------------------
+
+# Dataframe with results
+df = pd.read_csv('results/hyperparameter_tuning_ce.csv', index_col=0)
+
+# Summary of hyperparameter tuning
+df_summary = pd.read_csv('results/summary_tuning_ce.csv', index_col=0)
+
+# Names of hyperparameter columns of dataframe
+hyper_columns = hyperparameter_columns(df)
+
+# -----------------------------------------------------------------------------
+
+label_aug = {
+    'astro': ["Astro-aug", "astro_aug"],
+    'simclr': ["Simclr-aug", "simclr_aug"],
+    'jitter_simclr': ["Jitter-simclr", "jitter_simclr"],
+    'jitter_astro': ["Jitter-astro", "jitter_astro"],
+    'crop_simclr': ["Crop-simclr", "crop_simclr"],
+    'crop_astro': ["Crop-astro", "crop_astro"],
+    'rotation': ["Rotation", "rotation"],
+    'blur' : ["Blur", "blur"],
+    'without_aug' : ["Without-aug", "without_aug"]
+    }
+
+label_features = {
+    True: ["with features", "with_features"],
+    False: ["without features", "without_features"]
+    }
 
 # -----------------------------------------------------------------------------
 
@@ -33,8 +67,8 @@ def training_ce(args):
     # Early stop criterion
     early_stop_callback = EarlyStopping(
         monitor="accuracy_val",
-        min_delta=0.001,
-        patience=70,
+        min_delta=0.002,
+        patience=30,
         mode="max",
         check_finite=True,
         divergence_threshold=0.1
@@ -64,7 +98,7 @@ def training_ce(args):
 
     # Trainer
     trainer = pl.Trainer(
-        max_epochs=350,
+        max_epochs=180,
         gpus=gpus,
         benchmark=True,
         stochastic_weight_avg=False,
@@ -92,365 +126,86 @@ def training_ce(args):
     return (acc_val, conf_mat_val), (acc_test, conf_mat_test)
 
 # -----------------------------------------------------------------------------
-
-# with_features   : True
-# balanced_batch  : False
-# augmentation    : False
-
 # -----------------------------------------------------------------------------
 
-# Hyperparameters
-args = Box({"image_size": 21,
-            "batch_size": 75,
-            "drop_rate": 0.2,
-            "beta_loss": 0.512975,             
-            "lr": 0.001503,
-            "optimizer": "AdamW",
-            "with_features": True,
-            "balanced_batch": False,
-            "augmentation": False
-            })
+# Dataframe of the best hyperparameters for each combination in hyper
+df_best_ce = df_summary.loc[df_summary['mean'].idxmax()].reset_index(drop=True)
 
-# Save accuracies and confusion matrixes for different initial conditions
-acc_array_val = []
-conf_mat_array_val = []
-acc_array_test = []
-conf_mat_array_test = []
+# Mask of the best hyperparamter combination
+where = 1
+for hyper_name, opt_hyper in zip(hyper_columns, df_best_ce):
+    where = where & (df[hyper_name] == opt_hyper)
 
-# Train for different initial conditions
-for _ in range(5):
+# Folders of the best hyperparameter combination
+folder = list(df['logdir'][where])[0]
 
-    # Train and compute metrics
-    (acc_val, conf_mat_val), (acc_test, conf_mat_test) = training_ce(args)
+# Load the hyperparamters of the model
+hparams_path = os.path.join(folder, "hparams.yaml")
+hparams_file = open(hparams_path, 'r')
+hparams = yaml.load(hparams_file, Loader=yaml.FullLoader)
 
-    # Save metrics
-    acc_array_val.append(acc_val)
-    conf_mat_array_val.append(conf_mat_val)
-    acc_array_test.append(acc_test)
-    conf_mat_array_test.append(conf_mat_test)
-
-
-# Compute mean and standard deviation of accuracy and confusion matrix
-acc_mean_val = np.mean(acc_array_val, axis=0)
-acc_std_val = np.std(acc_array_val, axis=0)
-conf_mat_mean_val = np.mean(conf_mat_array_val, axis=0)
-conf_mat_std_val = np.std(conf_mat_array_val, axis=0)
-
-acc_mean_test = np.mean(acc_array_test, axis=0)
-acc_std_test = np.std(acc_array_test, axis=0)
-conf_mat_mean_test = np.mean(conf_mat_array_test, axis=0)
-conf_mat_std_test = np.std(conf_mat_array_test, axis=0)
-
-
-# Plot confusion matrix (validation)
-# ---------------------------------
-title = f"""Confusion matrix Stamps classifier
-(with features, without augmentations)
-Accuracy Validation:{acc_mean_val:.3f}$\pm${acc_std_val:.3f}"""
-file = 'Figures/confusion_matrix_CE-Validation-with_features-without_augmentation.png'
-plot_confusion_matrix_mean_std(conf_mat_mean_val, conf_mat_std_val, title, file)
-
-# Plot confusion matrix (test)
-# ----------------------------
-title = f"""Confusion matrix Stamps classifier
-(with features, without augmentations)
-Accuracy Test:{acc_mean_test:.3f}$\pm${acc_std_test:.3f}"""
-file = 'Figures/confusion_matrix_CE-Test-with_features-without_augmentation.png'
-plot_confusion_matrix_mean_std(conf_mat_mean_test, conf_mat_std_test, title, file)
-
+# Copy the best hyperparameters's set
+shutil.move(hparams_path, "results/hparams_best_ce.yaml")
 
 # -----------------------------------------------------------------------------
-
-# with_features   : False
-# balanced_batch  : False
-# augmentation    : False
-
 # -----------------------------------------------------------------------------
 
-# Hyperparameters
-args["with_features"] = False
-args["augmentation"] = False       
+# Train for different augmentations
+for augmentation in label_aug.keys():
+    for with_features in label_features.keys():
 
-# Save accuracies and confusion matrixes for different initial conditions
-acc_array_val = []
-conf_mat_array_val = []
-acc_array_test = []
-conf_mat_array_test = []
+        # Save accuracies and confusion matrixes for different initial conditions
+        acc_array_val = []
+        conf_mat_array_val = []
+        acc_array_test = []
+        conf_mat_array_test = []
 
-# Train for different initial conditions
-for _ in range(5):
+        # Train for different initial conditions
+        for _ in range(5):
 
-    # Train and compute metrics
-    (acc_val, conf_mat_val), (acc_test, conf_mat_test) = training_ce(args)
+            hparams['augmentation'] = augmentation
+            hparams['with_features'] = with_features
 
-    # Save metrics
-    acc_array_val.append(acc_val)
-    conf_mat_array_val.append(conf_mat_val)
-    acc_array_test.append(acc_test)
-    conf_mat_array_test.append(conf_mat_test)
+            # Load weights
+            sce = Supervised_Cross_Entropy(**hparams)
 
+            # Train and compute metrics
+            (acc_val, conf_mat_val), (acc_test, conf_mat_test) = training_ce(sce)
 
-# Compute mean and standard deviation of accuracy and confusion matrix
-acc_mean_val = np.mean(acc_array_val, axis=0)
-acc_std_val = np.std(acc_array_val, axis=0)
-conf_mat_mean_val = np.mean(conf_mat_array_val, axis=0)
-conf_mat_std_val = np.std(conf_mat_array_val, axis=0)
-
-acc_mean_test = np.mean(acc_array_test, axis=0)
-acc_std_test = np.std(acc_array_test, axis=0)
-conf_mat_mean_test = np.mean(conf_mat_array_test, axis=0)
-conf_mat_std_test = np.std(conf_mat_array_test, axis=0)
+            # Save metrics
+            acc_array_val.append(acc_val)
+            conf_mat_array_val.append(conf_mat_val)
+            acc_array_test.append(acc_test)
+            conf_mat_array_test.append(conf_mat_test)
 
 
-# Plot confusion matrix (validation)
-# ---------------------------------
-title = f"""Confusion matrix Stamps classifier
+        # Compute mean and standard deviation of accuracy and confusion matrix
+        acc_mean_val = np.mean(acc_array_val, axis=0)
+        acc_std_val = np.std(acc_array_val, axis=0)
+        conf_mat_mean_val = np.mean(conf_mat_array_val, axis=0)
+        conf_mat_std_val = np.std(conf_mat_array_val, axis=0)
+
+        acc_mean_test = np.mean(acc_array_test, axis=0)
+        acc_std_test = np.std(acc_array_test, axis=0)
+        conf_mat_mean_test = np.mean(conf_mat_array_test, axis=0)
+        conf_mat_std_test = np.std(conf_mat_array_test, axis=0)
+
+
+        # Plot confusion matrix (validation)
+        # ---------------------------------
+        title = f"""Confusion matrix Stamps classifier
 (without features, without augmentations)
+({label_features[with_features][0]}, {label_aug[augmentation][0]})
 Accuracy Validation:{acc_mean_val:.3f}$\pm${acc_std_val:.3f}"""
-file = 'Figures/confusion_matrix_CE-Validation-without_features-without_augmentation.png'
-plot_confusion_matrix_mean_std(conf_mat_mean_val, conf_mat_std_val, title, file)
+        file = f"figures/confusion_matrix_CE-Validation-{label_features[with_features][1]}-{label_aug[augmentation][1]}.png"
+        plot_confusion_matrix_mean_std(conf_mat_mean_val, conf_mat_std_val, title, file)
 
-# Plot confusion matrix (test)
-# ----------------------------
-title = f"""Confusion matrix Stamps classifier
-(without features, without augmentations)
+        # Plot confusion matrix (test)
+        # ----------------------------
+        title = f"""Confusion matrix Stamps classifier
+({label_features[with_features][0]}, {label_aug[augmentation][0]})
 Accuracy Test:{acc_mean_test:.3f}$\pm${acc_std_test:.3f}"""
-file = 'Figures/confusion_matrix_CE-Test-without_features-without_augmentation.png'
-plot_confusion_matrix_mean_std(conf_mat_mean_test, conf_mat_std_test, title, file)
-
-# -----------------------------------------------------------------------------
-
-# with_features   : True
-# balanced_batch  : False
-# augmentation    : 'default'
-
-# -----------------------------------------------------------------------------
-
-# Hyperparameters
-args["with_features"] = True
-args["augmentation"] = 'default'
-
-# Save accuracies and confusion matrixes for different initial conditions
-acc_array_val = []
-conf_mat_array_val = []
-acc_array_test = []
-conf_mat_array_test = []
-
-# Train for different initial conditions
-for _ in range(5):
-
-    # Train and compute metrics
-    (acc_val, conf_mat_val), (acc_test, conf_mat_test) = training_ce(args)
-
-    # Save metrics
-    acc_array_val.append(acc_val)
-    conf_mat_array_val.append(conf_mat_val)
-    acc_array_test.append(acc_test)
-    conf_mat_array_test.append(conf_mat_test)
-
-
-# Compute mean and standard deviation of accuracy and confusion matrix
-acc_mean_val = np.mean(acc_array_val, axis=0)
-acc_std_val = np.std(acc_array_val, axis=0)
-conf_mat_mean_val = np.mean(conf_mat_array_val, axis=0)
-conf_mat_std_val = np.std(conf_mat_array_val, axis=0)
-
-acc_mean_test = np.mean(acc_array_test, axis=0)
-acc_std_test = np.std(acc_array_test, axis=0)
-conf_mat_mean_test = np.mean(conf_mat_array_test, axis=0)
-conf_mat_std_test = np.std(conf_mat_array_test, axis=0)
-
-
-# Plot confusion matrix (validation)
-# ---------------------------------
-title = f"""Confusion matrix Stamps classifier
-(with features, SimCLR augmentations)
-Accuracy Validation:{acc_mean_val:.3f}$\pm${acc_std_val:.3f}"""
-file = 'Figures/confusion_matrix_CE-Validation-with_features-SimCLR_augmentation.png'
-plot_confusion_matrix_mean_std(conf_mat_mean_val, conf_mat_std_val, title, file)
-
-# Plot confusion matrix (test)
-# ----------------------------
-title = f"""Confusion matrix Stamps classifier
-(with features, SimCLR augmentations)
-Accuracy Test:{acc_mean_test:.3f}$\pm${acc_std_test:.3f}"""
-file = 'Figures/confusion_matrix_CE-Test-with_features-SimCLR_augmentation.png'
-plot_confusion_matrix_mean_std(conf_mat_mean_test, conf_mat_std_test, title, file)
-
-# -----------------------------------------------------------------------------
-
-# with_features   : False
-# balanced_batch  : False
-# augmentation    : 'default'
-
-# -----------------------------------------------------------------------------
-
-# Hyperparameters
-args["with_features"] = False
-args["augmentation"] = 'default'
-
-# Save accuracies and confusion matrixes for different initial conditions
-acc_array_val = []
-conf_mat_array_val = []
-acc_array_test = []
-conf_mat_array_test = []
-
-# Train for different initial conditions
-for _ in range(5):
-
-    # Train and compute metrics
-    (acc_val, conf_mat_val), (acc_test, conf_mat_test) = training_ce(args)
-
-    # Save metrics
-    acc_array_val.append(acc_val)
-    conf_mat_array_val.append(conf_mat_val)
-    acc_array_test.append(acc_test)
-    conf_mat_array_test.append(conf_mat_test)
-
-
-# Compute mean and standard deviation of accuracy and confusion matrix
-acc_mean_val = np.mean(acc_array_val, axis=0)
-acc_std_val = np.std(acc_array_val, axis=0)
-conf_mat_mean_val = np.mean(conf_mat_array_val, axis=0)
-conf_mat_std_val = np.std(conf_mat_array_val, axis=0)
-
-acc_mean_test = np.mean(acc_array_test, axis=0)
-acc_std_test = np.std(acc_array_test, axis=0)
-conf_mat_mean_test = np.mean(conf_mat_array_test, axis=0)
-conf_mat_std_test = np.std(conf_mat_array_test, axis=0)
-
-
-# Plot confusion matrix (validation)
-# ---------------------------------
-title = f"""Confusion matrix Stamps classifier
-(without features, SimCLR augmentations)
-Accuracy Validation:{acc_mean_val:.3f}$\pm${acc_std_val:.3f}"""
-file = 'Figures/confusion_matrix_CE-Validation-without_features-SimCLR_augmentation.png'
-plot_confusion_matrix_mean_std(conf_mat_mean_val, conf_mat_std_val, title, file)
-
-# Plot confusion matrix (test)
-# ----------------------------
-title = f"""Confusion matrix Stamps classifier
-(without features, SimCLR augmentations)
-Accuracy Test:{acc_mean_test:.3f}$\pm${acc_std_test:.3f}"""
-file = 'Figures/confusion_matrix_CE-Test-without_features-SimCLR_augmentation.png'
-plot_confusion_matrix_mean_std(conf_mat_mean_test, conf_mat_std_test, title, file)
-
-# -----------------------------------------------------------------------------
-
-# with_features   : True
-# balanced_batch  : False
-# augmentation    : 'astro'
-
-# -----------------------------------------------------------------------------
-
-args["with_features"] = True
-args["augmentation"] = 'astro'
-
-# Save accuracies and confusion matrixes for different initial conditions
-acc_array_val = []
-conf_mat_array_val = []
-acc_array_test = []
-conf_mat_array_test = []
-
-# Train for different initial conditions
-for _ in range(5):
-
-    # Train and compute metrics
-    (acc_val, conf_mat_val), (acc_test, conf_mat_test) = training_ce(args)
-
-    # Save metrics
-    acc_array_val.append(acc_val)
-    conf_mat_array_val.append(conf_mat_val)
-    acc_array_test.append(acc_test)
-    conf_mat_array_test.append(conf_mat_test)
-
-
-# Compute mean and standard deviation of accuracy and confusion matrix
-acc_mean_val = np.mean(acc_array_val, axis=0)
-acc_std_val = np.std(acc_array_val, axis=0)
-conf_mat_mean_val = np.mean(conf_mat_array_val, axis=0)
-conf_mat_std_val = np.std(conf_mat_array_val, axis=0)
-
-acc_mean_test = np.mean(acc_array_test, axis=0)
-acc_std_test = np.std(acc_array_test, axis=0)
-conf_mat_mean_test = np.mean(conf_mat_array_test, axis=0)
-conf_mat_std_test = np.std(conf_mat_array_test, axis=0)
-
-
-# Plot confusion matrix (validation)
-# ---------------------------------
-title = f"""Confusion matrix Stamps classifier
-(with features, Astro augmentations)
-Accuracy Validation:{acc_mean_val:.3f}$\pm${acc_std_val:.3f}"""
-file = 'Figures/confusion_matrix_CE-Validation-with_features-Astro_augmentation.png'
-plot_confusion_matrix_mean_std(conf_mat_mean_val, conf_mat_std_val, title, file)
-
-# Plot confusion matrix (test)
-# ----------------------------
-title = f"""Confusion matrix Stamps classifier
-(with features, Astro augmentations)
-Accuracy Test:{acc_mean_test:.3f}$\pm${acc_std_test:.3f}"""
-file = 'Figures/confusion_matrix_CE-Test-with_features-Astro_augmentation.png'
-plot_confusion_matrix_mean_std(conf_mat_mean_test, conf_mat_std_test, title, file)
-
-# -----------------------------------------------------------------------------
-
-# with_features   : False
-# balanced_batch  : False
-# augmentation    : 'astro'
-
-# -----------------------------------------------------------------------------
-
-# Hyperparameters
-args["with_features"] = False
-args["augmentation"] = 'astro'
-
-# Save accuracies and confusion matrixes for different initial conditions
-acc_array_val = []
-conf_mat_array_val = []
-acc_array_test = []
-conf_mat_array_test = []
-
-# Train for different initial conditions
-for _ in range(5):
-
-    # Train and compute metrics
-    (acc_val, conf_mat_val), (acc_test, conf_mat_test) = training_ce(args)
-
-    # Save metrics
-    acc_array_val.append(acc_val)
-    conf_mat_array_val.append(conf_mat_val)
-    acc_array_test.append(acc_test)
-    conf_mat_array_test.append(conf_mat_test)
-
-
-# Compute mean and standard deviation of accuracy and confusion matrix
-acc_mean_val = np.mean(acc_array_val, axis=0)
-acc_std_val = np.std(acc_array_val, axis=0)
-conf_mat_mean_val = np.mean(conf_mat_array_val, axis=0)
-conf_mat_std_val = np.std(conf_mat_array_val, axis=0)
-
-acc_mean_test = np.mean(acc_array_test, axis=0)
-acc_std_test = np.std(acc_array_test, axis=0)
-conf_mat_mean_test = np.mean(conf_mat_array_test, axis=0)
-conf_mat_std_test = np.std(conf_mat_array_test, axis=0)
-
-
-# Plot confusion matrix (validation)
-# ---------------------------------
-title = f"""Confusion matrix Stamps classifier
-(without features, Astro augmentations)
-Accuracy Validation:{acc_mean_val:.3f}$\pm${acc_std_val:.3f}"""
-file = 'Figures/confusion_matrix_CE-Validation-without_features-Astro_augmentation.png'
-plot_confusion_matrix_mean_std(conf_mat_mean_val, conf_mat_std_val, title, file)
-
-# Plot confusion matrix (test)
-# ----------------------------
-title = f"""Confusion matrix Stamps classifier
-(without features, Astro augmentations)
-Accuracy Test:{acc_mean_test:.3f}$\pm${acc_std_test:.3f}"""
-file = 'Figures/confusion_matrix_CE-Test-without_features-Astro_augmentation.png'
-plot_confusion_matrix_mean_std(conf_mat_mean_test, conf_mat_std_test, title, file)
+        file = f"figures/confusion_matrix_CE-Test-{label_features[with_features][1]}-{label_aug[augmentation][1]}.png"
+        plot_confusion_matrix_mean_std(conf_mat_mean_test, conf_mat_std_test, title, file)
 
 # -----------------------------------------------------------------------------
