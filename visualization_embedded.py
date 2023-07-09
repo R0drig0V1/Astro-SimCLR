d241af454249980547c3ade64fb94a8fc6adbbed
Rodrigo Vidal <arodrigovidal@gmail.com>
2023-07-08 23:30:10 -0400

    Visualization with umap is implemented and redundant .py files are removed

---
+++ visualization_embedded.py
@@ -0,0 +1,50 @@
+import os
+
+import pytorch_lightning as pl
+
+from utils.training import SimCLR
+from pytorch_lightning.callbacks import ModelCheckpoint
+
+# -----------------------------------------------------------------------------
+
+import logging
+logging.getLogger("pytorch_lightning").setLevel(logging.WARNING)
+
+import warnings
+warnings.filterwarnings("ignore")
+
+# -----------------------------------------------------------------------------
+
+class ModelCheckpoint_V2(ModelCheckpoint):
+    def _get_metric_interpolated_filepath_name(self, monitor_candidates, trainer, del_filepath) -> str:
+        return self.format_checkpoint_name(monitor_candidates)
+
+# -----------------------------------------------------------------------------
+
+simclr_augs = ["astro2", "astro8", "simclr", "simclr2"]
+encoder = "resnet18"
+image_size = 27
+
+# -----------------------------------------------------------------------------
+
+for simclr_aug in simclr_augs:
+    for rep in range(1):
+
+        # Load weights
+        simclr = SimCLR.load_from_checkpoint(f"../weights/SimCLR_{encoder}_{image_size}_{simclr_aug}/checkpoint_{rep}.ckpt")
+
+        # Load dataset
+        simclr.prepare_data_fast()
+
+        # Plot visualization (validation)
+        # ---------------------------------
+        #file = f'figures/tsne_Validation_{encoder}_{image_size}_{simclr_aug}_{rep}.png'
+        #simclr.plot_tSNE('Validation', file, feats_in_plot=100)
+
+        # Plot visualization (test)
+        # ----------------------------
+        file = f'figures/Visualization_Test_{encoder}_{image_size}_{simclr_aug}_{rep}.png'
+        simclr.visualization('Test', file)
+
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
