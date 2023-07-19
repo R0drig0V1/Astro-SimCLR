29329a25237b78f8b879b2f14949a9bf4ac097cc
Rodrigo Vidal <arodrigovidal@gmail.com>
2023-07-19 16:14:37 -0400

    Accuracy in percent base

--- iter_ce_resnet.py
+++ iter_ce_resnet.py
@@ -25,34 +25,41 @@ warnings.filterwarnings("ignore")
 
 # -----------------------------------------------------------------------------
 
-gpus = [2]
+gpus = [0]
 
 # -----------------------------------------------------------------------------
 
+# labels of encoders
+label_encoder = {
+    'pstamps': 'Stamps',
+    'resnet18': 'Resnet18',
+    'resnet34': 'Resnet34',
+    'resnet50': 'Resnet50',
+    'resnet152': 'Resnet152'
+}
+
 label_aug = {
-    #'astro'                        : ["Astro-aug",              "astro_aug"],
-    #'astro0'                       : ["Astro-aug-v0",           "astro_aug_v0"],
-    #'astro2'                       : ["Astro-aug-v2",           "astro_aug_v2"],
-    #'astro3'                       : ["Astro-aug-v3",           "astro_aug_v3"],
-    #'astro4'                       : ["Astro-aug-v4",           "astro_aug_v4"],
-    #'astro5'                       : ["Astro-aug-v5",           "astro_aug_v5"],
-    #'astro6'                       : ["Astro-aug-v6",           "astro_aug_v6"],
-    #'astro7'                       : ["Astro-aug-v7",           "astro_aug_v7"],
-    #'astro8'                       : ["Astro-aug-v8",           "astro_aug_v8"],
-    #'astro9'                       : ["Astro-aug-v9",           "astro_aug_v9"],
-    #'simclr'                       : ["Simclr-aug",             "simclr_aug"],
-    #'simclr2'                      : ["Simclr-aug-v2",          "simclr_aug_v2"],
-    #'simclr3'                      : ["Simclr-aug-v3",          "simclr_aug_v3"]
+    #'astro'                        : ["Astro",                  "astro"],
+    #'astro0'                       : ["Astro V0",               "astro0"],
+    #'astro2'                       : ["Astro V2",               "astro2"],
+    #'astro3'                       : ["Astro V3",               "astro3"],
+    #'astro4'                       : ["Astro V4",               "astro4"],
+    #'astro5'                       : ["Astro V5",               "astro5"],
+    #'astro6'                       : ["Astro V6",               "astro6"],
+    #'astro7'                       : ["Astro V7",               "astro7"],
+    #'astro8'                       : ["Astro V8",               "astro8"],
+    #'astro9'                       : ["Astro V9",               "astro9"],
+    #'simclr'                       : ["Simclr",                 "simclr"],
+    #'simclr2'                      : ["Simclr V2",              "simclr2"],
+    #'simclr3'                      : ["Simclr V3",              "simclr3"]
     #'jitter_simclr'                : ["Jitter-simclr",          "jitter_simclr"],
     #'jitter_astro'                 : ["Jitter-astro",           "jitter_astro"],
-    #'jitter_astro_v2'              : ["Jitter-astro v2",        "jitter_astro_v2"],
-    #'jitter_astro_v3'              : ["Jitter-astro v3",        "jitter_astro_v3"],
+    #'jitter_astro_v2'              : ["Jitter-astro V2",        "jitter_astro_v2"],
+    #'jitter_astro_v3'              : ["Jitter-astro V3",        "jitter_astro_v3"],
     #'gray_scale'                   : ["Gray-scale",             "gray_scale"],
     #'crop_simclr'                  : ["Crop-simclr",            "crop_simclr"],
     #'crop_astro'                   : ["Crop-astro",             "crop_astro"],
-    #'rotation'                     : ["Rotation",               "rotation"],
-    #'rotation_v2'                  : ["Rotation-v2",            "rotation_v2"],
-    #'rotation_v3'                  : ["Rotation-v3",            "rotation_v3"],
+    'rotation_v3'                  : ["Rotation",               "rotation"],
     #'crop_rotation'                : ["Crop-rotation",          "crop_rotation"],
     #'blur'                         : ["Blur",                   "blur"],
     #'perspective'                  : ["Random perspective",     "pers"],
@@ -68,12 +75,12 @@ label_aug = {
     #'elastic_prespective'          : ["Elastic-Perspective",    "elastic_pers"],
     #'grid_perspective'             : ["Grid-Perspective",       "grid_pers"],
     #'rot_elastic_grid_perspective' : ["Rot-Elastic-Grid-Pers",  "rot_elastic_grid_pers"],
-    'without_aug'                  : ["Without-aug",       "without_aug"]
+    'without_aug'                  : ["Without augmentation",    "without_aug"]
     }
 
 label_features = {
-    True: ["With features", "with_features"],
-    False: ["Without features", "without_features"]
+    True: ["With metadata", "with_metadata"],
+    False: ["Without metadata", "without_metadata"]
     }
 
 # -----------------------------------------------------------------------------
@@ -84,7 +91,7 @@ class ModelCheckpoint_V2(ModelCheckpoint):
 
 # -----------------------------------------------------------------------------
 
-def training_ce(hparams, name_checkpoint, name_tb, data_path):
+def training_ce(hparams, encoder, name_checkpoint, name_tb, data_path):
 
     # Early stop criterion
     early_stop_callback = EarlyStopping(
@@ -97,7 +104,7 @@ def training_ce(hparams, name_checkpoint, name_tb, data_path):
     # Save checkpoint
     checkpoint_callback = ModelCheckpoint_V2(
         monitor="accuracy_val",
-        dirpath=os.path.join(config.model_path, "CE_resnet18_red"),
+        dirpath=os.path.join(config.model_path, label_encoder[encoder]),
         filename=f"checkpoint_{name_checkpoint}",
         save_top_k=1,
         mode="max"
@@ -106,12 +113,12 @@ def training_ce(hparams, name_checkpoint, name_tb, data_path):
     # Define the logger object
     tb_logger = TensorBoardLogger(
         save_dir='tb_logs',
-        name='ce_resnet18_red',
+        name=label_encoder[encoder],
         version=name_tb
     )
 
     # Initialize model
-    model = Supervised_Cross_Entropy_Resnet(**hparams, data_path=data_path, resnet_model='resnet18')
+    model = Supervised_Cross_Entropy_Resnet(**hparams, data_path=data_path, resnet_model=encoder)
 
     # Trainer
     trainer = pl.Trainer(
@@ -120,7 +127,6 @@ def training_ce(hparams, name_checkpoint, name_tb, data_path):
         benchmark=True,
         callbacks=[checkpoint_callback, early_stop_callback],
         logger=tb_logger,
-        weights_summary=None
     )
 
 
@@ -146,75 +152,79 @@ def training_ce(hparams, name_checkpoint, name_tb, data_path):
 # -----------------------------------------------------------------------------
 
 # Load the hyperparamters of the model
-#hparams_file = open("results/hparams_best_ce_resnet.yaml", 'r')
 hparams_file = open("results/hparams_best_ce.yaml", 'r')
 hparams = Box(yaml.load(hparams_file, Loader=yaml.FullLoader))
 
 # -----------------------------------------------------------------------------
+
+encoders = ["resnet18"]
+image_size = 27
+
 # -----------------------------------------------------------------------------
 
 paths = {1: "dataset/td_ztf_stamp_17_06_20_sup_1.pkl",
          10: "dataset/td_ztf_stamp_17_06_20_sup_10.pkl",
-         100: "dataset/td_ztf_stamp_17_06_20_sup_100.pkl"
+         100: "dataset/td_ztf_stamp_17_06_20.pkl"
          }
 
 # -----------------------------------------------------------------------------
 
 # Train for different augmentations
-for augmentation in tqdm(label_aug.keys(), desc='Augmentations', unit= "aug"):
-    for with_features in tqdm(label_features.keys(), leave=False, desc='Features', unit="feat"):
-        for p in tqdm(paths.keys(), desc='dataset\'s fraction', unit= "p"):
+for encoder in tqdm(encoders, desc='Encoder', unit= "enc"):
+    for augmentation in tqdm(label_aug.keys(), desc='Augmentations', unit= "aug"):
+        for with_features in tqdm(label_features.keys(), leave=False, desc='Features', unit="feat"):
+            for p in tqdm(paths.keys(), desc='dataset\'s fraction', unit= "p"):
 
-            # Save accuracies and confusion matrixes for different initial conditions
-            acc_array_val = []
-            conf_mat_array_val = []
-            acc_array_test = []
-            conf_mat_array_test = []
+                # Save accuracies and confusion matrixes for different initial conditions
+                acc_array_val = []
+                conf_mat_array_val = []
+                acc_array_test = []
+                conf_mat_array_test = []
 
-            # Train for different initial conditions
-            for rep in tqdm(range(5), leave=False, desc='Repetion', unit="rep"):
+                # Train for different initial conditions
+                for rep in tqdm(range(5), leave=False, desc='Repetion', unit="rep"):
 
-                hparams.augmentation = augmentation
-                hparams.with_features = with_features
+                    hparams.augmentation = augmentation
+                    hparams.with_features = with_features
 
-                # Train and compute metrics
-                name_checkpoint = f"{p}_{augmentation}_{label_features[with_features][0]}_{rep+1}"
-                name_tb = f"{p}_{augmentation}_{label_features[with_features][0]}"
-                (acc_val, conf_mat_val), (acc_test, conf_mat_test) = training_ce(hparams, name_checkpoint, name_tb, paths[p])
+                    # Train and compute metrics
+                    name_checkpoint = f"{p}_{augmentation}_{label_features[with_features][1]}_{rep}"
+                    name_tb = f"{p}_{augmentation}_{label_features[with_features][1]}_{rep}"
+                    (acc_val, conf_mat_val), (acc_test, conf_mat_test) = training_ce(hparams, encoder, name_checkpoint, name_tb, paths[p])
 
-                # Save metrics
-                acc_array_val.append(acc_val)
-                conf_mat_array_val.append(conf_mat_val)
-                acc_array_test.append(acc_test)
-                conf_mat_array_test.append(conf_mat_test)
-
-
-            # Compute mean and standard deviation of accuracy and confusion matrix
-            acc_mean_val = np.mean(acc_array_val, axis=0)
-            acc_std_val = np.std(acc_array_val, axis=0)
-            conf_mat_mean_val = np.mean(conf_mat_array_val, axis=0)
-            conf_mat_std_val = np.std(conf_mat_array_val, axis=0)
-
-            acc_mean_test = np.mean(acc_array_test, axis=0)
-            acc_std_test = np.std(acc_array_test, axis=0)
-            conf_mat_mean_test = np.mean(conf_mat_array_test, axis=0)
-            conf_mat_std_test = np.std(conf_mat_array_test, axis=0)
+                    # Save metrics
+                    acc_array_val.append(acc_val)
+                    conf_mat_array_val.append(conf_mat_val)
+                    acc_array_test.append(acc_test)
+                    conf_mat_array_test.append(conf_mat_test)
 
 
-            # Plot confusion matrix (validation)
-            # ---------------------------------
-            title = f"""Confusion matrix ResNet18 classifier (labels {p}%)
+                # Compute mean and standard deviation of accuracy and confusion matrix
+                acc_mean_val = np.mean(acc_array_val, axis=0)
+                acc_std_val = np.std(acc_array_val, axis=0)
+                conf_mat_mean_val = np.mean(conf_mat_array_val, axis=0)
+                conf_mat_std_val = np.std(conf_mat_array_val, axis=0)
+
+                acc_mean_test = np.mean(acc_array_test, axis=0)
+                acc_std_test = np.std(acc_array_test, axis=0)
+                conf_mat_mean_test = np.mean(conf_mat_array_test, axis=0)
+                conf_mat_std_test = np.std(conf_mat_array_test, axis=0)
+
+
+                # Plot confusion matrix (validation)
+                # ---------------------------------
+                title = f"""Confusion matrix {label_encoder[encoder]} (labels {p}%)
 ({label_features[with_features][0]}, {label_aug[augmentation][0]})
-Accuracy Validation:{acc_mean_val:.3f}$\pm${acc_std_val:.3f}"""
-            file = f"figures/confusion_matrix_CE_Resnet18-frac_{p}-Validation-{label_features[with_features][1]}-{label_aug[augmentation][1]}.png"
-            plot_confusion_matrix_mean_std(conf_mat_mean_val, conf_mat_std_val, title, file)
+Accuracy Validation:{100*acc_mean_val:.1f}$\pm${100*acc_std_val:.1f}"""
+                file = f"figures/{label_encoder[encoder]}-{p}-Validation-{label_features[with_features][1]}-{label_aug[augmentation][1]}.png"
+                plot_confusion_matrix_mean_std(conf_mat_mean_val, conf_mat_std_val, title, file)
 
-            # Plot confusion matrix (test)
-            # ----------------------------
-            title = f"""Confusion matrix ResNet18 classifier (labels {p}%)
+                # Plot confusion matrix (test)
+                # ----------------------------
+                title = f"""Confusion matrix {label_encoder[encoder]} (labels {p}%)
 ({label_features[with_features][0]}, {label_aug[augmentation][0]})
-Accuracy Test:{acc_mean_test:.3f}$\pm${acc_std_test:.3f}"""
-            file = f"figures/confusion_matrix_CE_Resnet18-frac_{p}-Test-{label_features[with_features][1]}-{label_aug[augmentation][1]}.png"
-            plot_confusion_matrix_mean_std(conf_mat_mean_test, conf_mat_std_test, title, file)
+Accuracy Test:{100*acc_mean_test:.1f}$\pm${100*acc_std_test:.1f}"""
+                file = f"figures/{label_encoder[encoder]}-{p}-Test-{label_features[with_features][1]}-{label_aug[augmentation][1]}.png"
+                plot_confusion_matrix_mean_std(conf_mat_mean_test, conf_mat_std_test, title, file)
 
 # -----------------------------------------------------------------------------
